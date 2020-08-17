d2b21462006fabfa7416707377254440f390f4b9
unknown <micwilk2010@hotmail.fr>
2020-08-17 12:04:29 +0200

    ajoue material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './Components/HelloWorld'
+import Tst from './Components/tst'
+
 
 function App() {
   return (
     <div>
       <HelloWorld />
+      <Tst />
     </div>
   );
 }
