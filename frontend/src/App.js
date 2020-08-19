1d3a8a5becb86b65bc3d7b2e05174dbd8ec8b5ab
unknown <micwilk2010@hotmail.fr>
2020-08-19 07:49:27 +0200

    structurisation des views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './tailwind.css';
 import HelloWorld from './Components/HelloWorld'
+import Home from "./Views/Home"
+import Register from "./Views/Register"
 import Tst from './Components/tst'
 import Concours from './Components/Concours'
+import NavigationMenu from './Components/NavigationMenu';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 }
   from "react-router-dom"
 
@@ -15,11 +17,24 @@ import {
 function App() {
   return (
     <div>
-      <HelloWorld />
-      <Tst />
       <Router>
+        <HelloWorld />
+        <Tst />
         <Concours />
+        <NavigationMenu />
+
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Register">
+            <Register />
+          </Route>
+        </Switch>
+
       </Router>
+
 
     </div>
   );
--- frontend/src/Components/Concours.js
+++ frontend/src/Components/Concours.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
- import {useParams} from "react-router-dom";
+// import {useParams} from "react-router-dom";
 
 
 function Concours() {
 
-        const {id} = useParams()
-    const url = `https://5f3be1fcfff8550016ae5d56.mockapi.io/Projet/concours/${id}`
+        //const {id} = useParams()
+    const url = "https://5f3be1fcfff8550016ae5d56.mockapi.io/Projet/concours/"
     const [concours, setConcours] = useState(null)
 
     useEffect(() => {
