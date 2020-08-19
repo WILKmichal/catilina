8049ed3cdcfdcce669ffa5dd5cda802bd79ae793
unknown <micwilk2010@hotmail.fr>
2020-08-19 11:09:43 +0200

    indentation

--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import HelloWorld from '../Components/HelloWorld'
 
 function Home() {
         return (
-                <div className = "border-b font-bold p-3">
+                <div className="border-b font-bold p-3">
                         <h1>
                                 Vous etes sur la page /Home
                         </h1>
+                        <HelloWorld />
                 </div>
         );
 }
