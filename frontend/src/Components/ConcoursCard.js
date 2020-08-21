02fd9ccc38c85bdb2bbce950073e1209b6005830
unknown <micwilk2010@hotmail.fr>
2020-08-21 16:37:57 +0200

    visual clue pour les dates de fin d'inscriptions

--- frontend/src/Components/ConcoursCard.js
+++ frontend/src/Components/ConcoursCard.js
@@ -1,8 +1,46 @@
 import React from "react";
 
 function ConcoursCard(props) {
+
+    let currentDate = new Date();
+    currentDate = currentDate.getTime();
+
+    let finDateParse = new Date(props.concour.date_fin);
+    let finDate = finDateParse.getTime()
+
+    let differenceJour = parseInt((finDate - currentDate) / 86400000)
+
+    let dateClassName
+    let finDateInfo
+
+    if (differenceJour <= 7) {
+        dateClassName = "bg-red-600 absolute rounded bg-opacity-50";
+        finDateInfo = "fin des inscription dans " + differenceJour;
+    }
+    else if (differenceJour <= 14 && differenceJour > 7) {
+        dateClassName = "bg-green-600 absolute rounded bg-opacity-50";
+        finDateInfo = "fin des inscription dans " + differenceJour;
+    }
+    else if (differenceJour <= 21 && differenceJour > 14) {
+        dateClassName = "bg-teal-600 absolute rounded bg-opacity-50";
+        finDateInfo = "fin des inscription dans " + differenceJour;
+    }
+    else if (differenceJour <= 31 && differenceJour > 21) {
+        dateClassName = "bg-blue-600 absolute rounded bg-opacity-50";
+        finDateInfo = "fin des inscription dans " + differenceJour;
+    }
+    else {
+        dateClassName = "bg-purple-600 absolute rounded bg-opacity-50"
+        finDateInfo = "fin des inscription le "+ finDateParse.getFullYear(2020) + "/" +(finDateParse.getMonth()+1) + "/"+finDateParse.getDate();
+    }
+
+
+
+
+
     return (
-        // <div>
+        //tout les props
+
 
         //      {props.concour.image}
         //     {props.concour.id_concours}
@@ -10,9 +48,11 @@ function ConcoursCard(props) {
         //     {props.concour.libel_l}
         //     {props.concour.date_fin}
         //     {props.concour.id_sous_theme}
-        //     
-        // </div>
+
         <div className="max-w-sm rounded overflow-hidden shadow-lg hover:opacity-50">
+            <span className={dateClassName}>
+                <p className="px-2 py-1"> {finDateInfo} </p>
+            </span>
             <img className="w-full" src={props.concour.image} alt="Sunset in the mountains"></img>
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{props.concour.libel_sh}</div>
--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ConcoursCardPlaceHolder from "../Components/ConcoursCardPlaceHolder"
+// import ConcoursCardPlaceHolder from "../Components/ConcoursCardPlaceHolder"
 import ListeAleatoireConcours from "../Components/ListeAleatoireConcours";
 
 function Home(){
     return(
         <div>
-            <ConcoursCardPlaceHolder />
+            {/* <ConcoursCardPlaceHolder /> */}
             <ListeAleatoireConcours />
         </div>
     )
