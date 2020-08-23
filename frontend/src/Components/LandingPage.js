9a20d4e0aad9e273a90c9831da75da25338a496e
unknown <micwilk2010@hotmail.fr>
2020-08-23 22:07:47 +0200

    debut bancale parallax

--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import {Parallax} from "react-parallax";
+import { Parallax } from "react-parallax";
 
 function LandingPage() {
     return (
         <div>
+
+            <Parallax
+                bgImage={require('./img/happy-bunch.png')}
+                bgImageAlt="the cat"
+                strength={200}
+                style={{
+                    left: '50%',
+                    top: '200px',
+                    backgroundsize: "contain",
+                    width: "200px",
+                    height: "100px"
+                }}
+            >
+            </Parallax>
+
+
+
             <div className="text-center font-sans">
                 {/* eslint-disable-next-line */}
                 <div className="h-32"></div>
@@ -12,7 +29,7 @@ function LandingPage() {
                 <a className="text-2xl tracking-tight font-semibold">lorem ipsum</a>
                 <div className="h-8"></div>
                 {/* eslint-disable-next-line */}
-                <a className="text-6xl font-extrabold tracking-widest">RANDOMISATION ANDOCRINIENT</a>
+                <span className="text-6xl font-extrabold tracking-widest">RANDOMISATION ANDOCRINIENT</span>
                 <div className="h-8"></div>
                 {/* eslint-disable-next-line */}
                 <a className="text-2xl tracking-tight font-semibold">bullshit descriptif again the zouk</a>
@@ -21,8 +38,8 @@ function LandingPage() {
                     < ArrowForwardIosIcon />
                     <span className="text-xl">Register</span>
                 </button>
+                <div className="h-32"></div>
             </div>
-
         </div>
     )
 }
