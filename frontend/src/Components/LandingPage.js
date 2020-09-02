4aba6fc71c7f3b1fd675d57c6d496679e1338d80
ericvillage <60705614+ericvillage@users.noreply.github.com>
2020-09-02 13:17:00 +0200

    test

--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -1,24 +1,27 @@
 import React from "react";
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import Demo from './PostuleButton.js';
+import Exam from './img/Exam.jpg';
+import Graduation from './img/Graduation.jpg'
 
 function LandingPage() {
     return (
-        <div>
-            <div className="text-center font-sans">
+        <div className="bg-cover bg-center opacity-50" style={{ backgroundImage:`url(${Graduation})` }}>
+            <div className="text-center text-white font-sans">
                 {/* eslint-disable-next-line */}
                 <div className="h-32"></div>
                 {/* eslint-disable-next-line */}
-                <a className="text-2xl tracking-tight font-semibold md:text-base">lorem ipsum</a>
+                <a className="text-base tracking-tight font-semibold md:text-base text-3xl">inscription concours en ligne</a>
                 <div className="h-8"></div>
                 {/* eslint-disable-next-line */}
-                <span className="text-6xl font-extrabold tracking-widest">RANDOMISATION ANDOCRINIENT</span>
+                <span className="text-4xl font-extrabold tracking-widest md:text-6xl ">MAXICONCOURS</span>
                 <div className="h-8"></div>
                 {/* eslint-disable-next-line */}
-                <a className="text-2xl tracking-tight font-semibold">bullshit descriptif again the zouk</a>
+                <a className="text-2xl tracking-tight font-semibold">Votre avenir à la portée d'un clic</a>
                 <div className="h-24"></div>
-                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold px-10 rounded inline-flex items-center py-6">
+                <button className="bg-teal-500 hover:bg-teal-700 text-white font-bold px-6 rounded inline-flex items-center py-3 z-40">
                     < ArrowForwardIosIcon />
-                    <span className="text-xl">Register</span>
+                    <span className="text-xl"><Demo /></span>
                 </button>
                 <div className="h-32"></div>
             </div>
