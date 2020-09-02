cced0f12c860fc1035e420a09f5d2fbb00f62dc6
tchejy <66727499+tchejy@users.noreply.github.com>
2020-09-02 16:30:58 +0200

    mise a jour boutton theme

--- frontend/src/Components/ThemeBoutton.js
+++ frontend/src/Components/ThemeBoutton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ThemeButton(props) {
     return (
-        <button className="p-1 bg-teal-500 text-gray-100 font-semibold border border-teal-600 rounded hover:border-transparent hover:bg-teal-600 hover:text-white hover:font-bold">
+        <button className="p-1 bg-teal-500 text-gray-100 font-semibold border border-teal-600 rounded hover:border-transparent hover:bg-teal-600 hover:text-white hover:font-bold h-24 w-full">
 
             {props.theme.nom_theme}
         </button>
