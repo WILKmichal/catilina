718c07be56197df23a77fc53166f3e0ddea42f36
unknown <micwilk2010@hotmail.fr>
2020-09-18 15:08:38 +0200

    ajoue backup sql

--- backend/routes/routesPublic/themeSous.js
+++ backend/routes/routesPublic/themeSous.js
@@ -1,1 +1,15 @@
-// TODO soustheme vide
+const router = require("express").Router();
+const db = require('../../db/SousthemeDb.js')
+// const loginValidation = require("../../models/schemaTheme")
+
+
+router.get('/theme', async (req, res) => {
+
+    let listeTheme = await db.listeSousTheme()
+
+    res.json(listeTheme)
+    return
+});
+
+
+module.exports = router;
